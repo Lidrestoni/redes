e94e29ac0361c22b3007e47fdfbea95244e52f09
lidrestoni <guilherme.antunes@uffs.edu.br>
2018-07-04 11:53:22 +0300

    Segundo trabalho uhu
    Things are working at last!

--- router.c
+++ router.c
@@ -17,17 +17,96 @@ Tipos de Mensangens:
 
 Atualização:
 001 | ID do remetente |Número de colunas X | Cabeçalho 1 | Conteúdo 1 |Cabeçalho 2| Conteúdo 2  ... Cabeçalho X | Conteúdo X | 
+
+UDP:
+000 | 
+
 */
 
 #include <pthread.h>
 #include <unistd.h>
 #include <sys/time.h>
 #include <time.h>
-#include "udp.h"
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdio.h> //printf
+#include <string.h> //memset
+#include <stdlib.h> //exit(0);
+
 #define vtMAX 1123
 #define totalMesLength 500
+#define INF 11234567
 
 int vetdist[vtMAX][vtMAX], vetColumndistN=0, vetLinedistN=0, vetColumnLabels[vtMAX], vetLineLabels[vtMAX], nextLine[vtMAX]/*O nextline tem a mesma ordenação do vetColumnLabels*/;
+
+char *meIP;
+int meID,mePORT, Socket; 
+struct sockaddr_in si_me;
+
+void die(char *s){
+	perror(s);
+	exit(1);
+}
+
+
+static int messageTotalLen = 3*sizeof(int)+101*sizeof(char);
+
+void PortsFromFile( int *PORT, int id, char **ip){
+	int t[2];
+	*ip = malloc(sizeof(char)*10);
+	FILE *fp = fopen("roteador.config", "r");
+	if(fp==NULL){
+		printf("Não foi possível abrir o arquivo \"roteador.config\"\n");		
+		exit(1);
+	}
+	do{
+		if(fscanf(fp,"%d %d %s ", &t[0], &t[1], *ip)==EOF){
+			printf("ERRO: O roteador %d não foi encontrado!", id);			
+			exit(1);
+		}
+		if(t[0]==id){
+			*PORT = t[1];		
+		}
+	}
+	while(*PORT==-1);
+	fclose(fp);
+}
+
+void configureAndBindMeStructures(char *id){
+	meID = atoi(id);
+	mePORT = -1;	
+	PortsFromFile(&mePORT, meID, &meIP);	//printf("<<%d>>", mePORT);
+
+	if ( (Socket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+	{
+		die("socket");
+	}
+
+	// zero out the structure
+	memset((char *) &si_me, 0, sizeof(si_me));
+
+	si_me.sin_family = AF_INET;
+	si_me.sin_port = htons(mePORT); 
+	//si_me.sin_addr.s_addr = htonl(atoi(meIP));//INADDR_ANY);//printf("{{{{%d:%d(%d)}}}}",si_me.sin_port, mePORT,si_me.sin_addr.s_addr);
+	si_me.sin_addr.s_addr = inet_addr(meIP);
+	//bind socket to port
+	if( bind(Socket , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1){
+		die("bind");
+	}
+}
+
+void getDestIPandPort(int destID,int *destPORT, char *destIP,struct sockaddr_in *si_dest){
+	*destPORT = -1;		
+	PortsFromFile(destPORT, destID, &destIP);
+	si_dest->sin_port = htons(*destPORT);//printf("\n{Port:%d-%d IP: %s}", *destPORT,si_dest->sin_port, destIP);
+	//printf("?{%d}-%d", si_dest->sin_port, *destPORT);
+	if (inet_aton(destIP , &si_dest->sin_addr) == 0){	
+		fprintf(stderr, "inet_aton() failed\n");
+		exit(1);
+	}
+}
+
+
 
 void printNextLine(){
 	int i, j;
@@ -172,6 +251,11 @@ void atualizaVD(){
 	}
 }
 
+struct UDPMessage{
+	int idMes,idOrig,idDest;
+	char mess[101];
+};
+
 void readMessage001(char *vet){ //Lê o vetor mandado por src e atualiza o vetor distância na linha correspondente.
 	if(vet[0]=='0'&&vet[1]=='0'&&vet[2]=='1'){
 		int i=4, xx=0, n, src, dst, k, srcID;
@@ -201,6 +285,7 @@ void readMessage001(char *vet){ //Lê o vetor mandado por src e atualiza o vetor
 					vetdist[getVetLineLabel(srcID)][dst]=atoi(x);}
 			}
 		}
+		atualizaVD();
 		
 	}
 	else{
@@ -208,11 +293,28 @@ void readMessage001(char *vet){ //Lê o vetor mandado por src e atualiza o vetor
 	}
 }
 
-void readMessage(char *mes){//printf("Reading :->%s<-:",mes);
-	if(mes[0]=='0'&&mes[1]=='0'&&mes[2]=='1')
-		readMessage001(mes);
+void readMessage010(char *a, struct UDPMessage *m){ //Mensagem utilizada para encaminhar a mensagem ao destino via UDP
+	if(a[0]=='0'&&a[1]=='1'&&a[2]=='0'){	
+		int i=4, x[3], v=3;
+		char *p, temp[103];
+		while(v--){
+			p = &a[i];
+			while(a[i]!=';')
+				i++;
+			a[i]='\0';
+			x[v] = atoi(p);
+			a[i]=';';
+			i++;
+		}
+		m->idMes=x[2]; m->idOrig = x[1]; m->idDest = x[0];
+		int j=0;
+		while(a[i]!=';'&&a[i]!='\0')
+			temp[j++]=a[i++];
+		temp[j]='\0';
+		strcpy(m->mess,temp);
+	}
 	else
-		printf("Warning: Foi recebida mensagem do tipo %c%c%c, que é desconhecido.\n", mes[0], mes[1], mes[2]);
+		printf("Warning: Erro de tratamento de mensagem! Uma mensagem do tipo %c%c%c está solicitando tratamento do tipo 010\n", a[0], a[1], a[2]);
 }
 
 
@@ -240,6 +342,17 @@ char* createMessage001(){ //Mensagem utilizada para atualizar o vetor distância
 	return mystr;
 	
 }
+
+
+
+
+
+char* createMessage010(struct UDPMessage m){//Mensagem utilizada para encaminhar a mensagem ao destino via UDP
+	char *apstr = malloc(messageTotalLen);
+	snprintf (apstr, messageTotalLen+1, "010|%d;%d;%d;%s;", m.idMes,m.idOrig, m.idDest, m.mess); 
+	return apstr;
+}
+
 
 
 void addEdgeToVD(int from, int to, int value){
@@ -274,7 +387,7 @@ void startVetDistFromFile(){
 }
 
 
-int destFinalID;
+
 
 static time_t TIMEOUT = 1; 
 /*Estrutura utilizada para conferir o timeout*/
@@ -394,48 +507,70 @@ int main2(int id){
 	int LastMessID=0;
 	if(id==0){//Thread responsável por receber mensagens dos vizinhos
 		struct sockaddr_in si_other;
-		int recv_len, other_len = sizeof(si_other);
+		int recv_len, other_len = sizeof(si_other), destIDLabel;
 		while(1){
 			memset(buf,'\0', BUFLEN);
 			//try to receive some data, this is a blocking call
 			if ((recv_len=recvfrom(Socket, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &other_len)) == -1)
 				die("recvfrom()");
 			//printf("Recebi \"%s\"\n", buf);
-			printf("Received packet{\"%s\"} from %s-%d:%d\n", buf,inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port),si_other.sin_port);
-			readMessage(buf);
-			printVetDist();
-			atualizaVD();
-			printVetDist();
-			printNextLine();
+			//printf("Received packet{\"%s\"} from %s-%d:%d\n", buf,inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port),si_other.sin_port);
+			if(buf[0]=='0'&&buf[1]=='0'&&buf[2]=='1')
+				readMessage001(buf);
+			else if(buf[0]=='0'&&buf[1]=='1'&&buf[2]=='0'){
+				readMessage010(buf, &mes);
+				if(mes.idDest==meID){
+					printf("Recebi minha mensagem!");return 0;				
+				}
+				else{
+					printNextLine();
+					destIDLabel = getVetLabel(vetColumnLabels, &vetColumndistN,&vetLinedistN,mes.idDest, 1);
+					getDestIPandPort(nextLine[destIDLabel],&destPORT, destIP,&si_dest);
+					printf("Nodo %d encaminhando mensagem #%d para o nodo %d, com destino final no nodo %d\n(O caminho percorrido aparecerá no outro terminal)\n", meID,mes.idMes,nextLine[destIDLabel], mes.idDest);
+					message = createMessage010(mes);
+					if (sendto(Socket, message, strlen(message) , 0 , (struct sockaddr *) &si_dest, slen)==-1)
+						die("sendto()");
+				}
+			}
+			else
+				printf("Warning: Foi recebida mensagem do tipo %c%c%c, que é desconhecido.\n", buf[0], buf[1], buf[2]);
+			//printVetDist();
+			//printVetDist();
+			//printNextLine();
 		}
 	}
-	else if(id==1)
+	else if(id==1){
+		int destIDLabel;
 		while(1){
 			printf("Para qual roteador mandar a mensagem? ");
 			do{
-				scanf("%d", &destFinalID);
+				scanf("%d", &destID);
 			}
-			while(destFinalID==meID||destFinalID<1);
+			while(destID==meID||destID<1);
 			getchar();
 			//resetupDestStructures(&destNextID,graph,destFinalID,&destPORT, destIP, &si_dest);
 			mes.idMes =  LastMessID++;
 			mes.idOrig = meID;
-			mes.idDest = destFinalID;
-			getDestIPandPort(nextLine[getVetLabel(vetColumnLabels, &vetColumndistN,&vetLinedistN,destFinalID , 1)],&destPORT, destIP,&si_dest);
+			mes.idDest = destID;
+			destIDLabel = getVetLabel(vetColumnLabels, &vetColumndistN,&vetLinedistN,destID , 1);
+			getDestIPandPort(nextLine[destIDLabel],&destPORT, destIP,&si_dest);
 			printf("Enter message : ");
 			//fflush(stdout);
 			getline(&message, &messageSize, stdin);
 			fflush(stdout); 
 			strcpy(mes.mess,message);
-			message = UDPMessageToStr(mes);
+			message = createMessage010(mes);
+			printNextLine();
        	 
 			//send the message
-			printf("Nodo %d encaminhando mensagem #%d para o nodo %d, com destino final no nodo %d\n(O caminho percorrido aparecerá no outro terminal)\n", meID,mes.idMes,destID, mes.idDest);
+			printf("Nodo %d encaminhando mensagem #%d para o nodo %d, com destino final no nodo %d\n(O caminho percorrido aparecerá no outro terminal)\n", meID,mes.idMes,destIDLabel, mes.idDest);
+			printf("[%d]", si_dest.sin_port);
 			addConfs(mes.idMes, message);
 			if (sendto(Socket, message, strlen(message) , 0 , (struct sockaddr *) &si_dest, slen)==-1){
 				die("sendto()");
 			}//else printf("[%d]", si_dest.sin_port);
 		}
+	}
 	else if(id==2){	/*Thread responsável por dar sinal de vida aos vizinhos*/
 		char *m;
 		while(1){
@@ -462,7 +597,7 @@ int main2(int id){
 					fprintf(stderr,"[%d]O pacote #%d deu timeout! Por isso será reenviado!\n",(int)time(NULL),it->idMes);
 					fflush(stdout);					
 					it->time = time(NULL);
-					StrToUDPMessage (it->arrayMes, &mes);
+					readMessage010(it->arrayMes, &mes);
 																         	
 					//send the message
 					printf("Nodo %d Reencaminhando mensagem #%d para o nodo %d, com destino final no nodo %d\n(O caminho percorrido aparecerá no outro terminal)\n", meID,mes.idMes,destID, mes.idDest);
